986cb2a1cc8ddf32a5ee73931724eaddec27d404
omriv88 <113102456+omriv88@users.noreply.github.com>
2022-12-17 10:46:34 +0200

    Update compose.sh

--- compose.sh
+++ compose.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker-compose -f /home/ec2-user/wordpress/docker-compose.yaml up --bulid -d
+#sudo docker-compose -f /home/ec2-user/wordpress/docker-compose.yaml up --bulid -d
+docker-compose up -d
